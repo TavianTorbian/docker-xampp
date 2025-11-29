793649cd42834950c8e803a8804e0495e51598ce
st10999 <st10999@iismarconipieralisi.it>
2025-11-29 11:36:16 +0000

    siii

--- src/TestForm/Test.php
+++ src/TestForm/Test.php
@@ -3,13 +3,10 @@
   echo "<br>";
 
   echo "Visualizzazione della varabile \$_REQUEST con echo:<br>";
-  //non sempre la echo è la scelta giusta!
-  //si aspetta una stringa!
   echo "$_REQUEST<br>";
   echo "<br>";
 
   echo "Visualizzazione della varabile \$_REQUEST con print_r():<br>";
-  //visualizza le informazioni in maniera human readable
   print_r($_REQUEST);
   echo "<br><br>";
 
@@ -23,8 +20,6 @@
     echo "GET:<br>";
     print_r($_GET);
 
-    //visualizza i dati ricevuti dalla post/get
-    //iterando gli elementi dell'array associativo
     foreach($_GET as $key=>$value)
     {
         echo "$key=$value";
@@ -36,8 +31,6 @@
     echo "POST:<br>";
     print_r($_POST);
 
-    //visualizza i dati ricevuti dalla post/get
-    //iterando gli elementi dell'array associativo
     foreach($_POST as $key=>$value)
     {
         echo "$key=$value";
