34bb8b0612b8bd1d97e731d13abc20ab3ae0f113
st10999 <st10999@iismarconipieralisi.it>
2025-11-29 11:32:57 +0000

    ora si

--- src/TestForm/Test.php
+++ src/TestForm/Test.php
@@ -2,10 +2,25 @@
   echo "Tipo di richiesta: " . $_SERVER['REQUEST_METHOD'] . "<br>";
   echo "<br>";
 
+  echo "Visualizzazione della varabile \$_REQUEST con echo:<br>";
+  //non sempre la echo è la scelta giusta!
+  //si aspetta una stringa!
+  echo "$_REQUEST<br>";
+  echo "<br>";
+
+  echo "Visualizzazione della varabile \$_REQUEST con print_r():<br>";
+  //visualizza le informazioni in maniera human readable
+  print_r($_REQUEST);
+  echo "<br><br>";
+
+  echo "Visualizzazione della varabile \$_REQUEST con var_dump():<br>";
+  var_dump($_REQUEST);
+  echo "<br><br>";
+
   echo "Contenuto della richiesta:<br>";
   if ($_SERVER['REQUEST_METHOD'] === 'GET')
   {
-    echo "GET: ";
+    echo "GET:<br>";
     print_r($_GET);
 
     //visualizza i dati ricevuti dalla post/get
@@ -18,7 +33,7 @@
   }
   elseif ($_SERVER['REQUEST_METHOD'] === 'POST')
   {
-    echo "POST: ";
+    echo "POST:";
     print_r($_POST);
 
     //visualizza i dati ricevuti dalla post/get
