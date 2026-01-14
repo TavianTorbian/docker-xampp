adbbabf53faa793118e8cc7d9d25c42845f24718
st10999 <st10999@iismarconipieralisi.it>
2026-01-14 12:02:38 +0000

    esperienza chatrrom inizio

---
+++ src/EsperianzaChatRoom/SignUp.php
@@ -0,0 +1,20 @@
+<?php
+    $host = 'db'; 
+    $dbname = 'ChatRoom'; 
+    $user = 'user';
+    $password = 'user';
+    $port = 3306;
+    
+    $connection = new mysqli($host, $user, $password, $dbname, $port);
+    
+    if ($connection->connect_error) 
+    {
+        die("Errore di connessione: " . $connection->connect_error);
+    }
+
+
+
+
+
+    $connection->close();
+?>
