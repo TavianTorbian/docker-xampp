64e9ecf74ad0332cccbc009b8c7992d463323d00
st10999 <st10999@iismarconipieralisi.it>
2026-01-19 07:43:44 +0000

    hash della password

--- src/EsperienzaChatRoom/Login.php
+++ src/EsperienzaChatRoom/Login.php
@@ -1,33 +1,41 @@
 <?php
-    session_start();
-    $host = 'db'; 
-    $dbname = 'ChatRoom'; 
-    $user = 'user';
-    $password = 'user';
-    $port = 3306;
-    
-    $connection = new mysqli($host, $user, $password, $dbname, $port);
-    
-    if ($connection->connect_error) 
-    {
-        die("Errore di connessione: " . $connection->connect_error);
-    }
-    
-    $username=htmlspecialchars($_POST['username']);
-    $password=htmlspecialchars($_POST['password']);
-    
-    $stmt = $connection->prepare("SELECT * FROM utenti WHERE username = ? AND password = ?");
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
+session_start();
 
-    if ($result-> num_rows > 0)
-    {
-      $_SESSION['username']=true;
-      header('Location: Pannello.php');
-    }else {
-      echo "Login non riuscito! <br>";
+$host = 'db';
+$dbname = 'ChatRoom';
+$user = 'user';
+$dbpassword = 'user';
+$port = 3306;
+
+$connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
+
+if ($connection->connect_error) {
+    die("Errore di connessione: " . $connection->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$stmt = $connection->prepare("SELECT password FROM utenti WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+    $row = $result->fetch_assoc();
+    $hashSalvato = $row['password'];
+
+    if (password_verify($password, $hashSalvato)) {
+        session_regenerate_id(true);
+        $_SESSION['username'] = $username;
+        header("Location: Pannello.php");
+    } else {
+        echo "Password errata!";
     }
-    
-    $connection->close();
+} else {
+    echo "Utente non trovato!";
+}
+
+$connection->close();
+
 ?>
--- src/EsperienzaChatRoom/SignUp.php
+++ src/EsperienzaChatRoom/SignUp.php
@@ -1,37 +1,41 @@
 <?php
-    $host = 'db'; 
-    $dbname = 'ChatRoom'; 
-    $user = 'user';
-    $password = 'user';
-    $port = 3306;
-    
-    $connection = new mysqli($host, $user, $password, $dbname, $port);
-    
-    if ($connection->connect_error) 
-    {
-        die("Errore di connessione: " . $connection->connect_error);
-    }
+session_start();
 
-    $username=htmlspecialchars($_POST['username']);
-    $password=htmlspecialchars($_POST['password']);
-    
-    $query = "SELECT * FROM utenti WHERE username = '$username'";
-    $result = $connection->query($query);
+$host = 'db';
+$dbname = 'ChatRoom';
+$user = 'user';
+$dbpassword = 'user';
+$port = 3306;
 
-    if($result->num_rows == 0)
-    {
-        $query = "INSERT INTO utenti(username, password) VALUES('$username','$password')";
-        $result = $connection->query($query);
-        $_SESSION['username']=true;
-        header('Location: Pannello.php');
-        if ($result-> num_rows > 0)
-        {
-            echo "Registrazione Effettuata! <br>";
-        }else{
-            echo "Registrazione non effettuata! <br>";
-            echo "Utente già presente! <br>";
-        }
-    }
+$connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
 
-    $connection->close();
+if ($connection->connect_error) {
+    die("Errore di connessione: " . $connection->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$stmt = $connection->prepare("SELECT * FROM utenti WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo "Utente già presente!";
+}
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+$stmt = $connection->prepare("INSERT INTO utenti (username, password) VALUES (?, ?)");
+$stmt->bind_param("ss", $username, $hash);
+
+if ($stmt->execute()) {
+    $_SESSION['username'] = $username;
+    header("Location: Pannello.php");
+} else {
+    echo "Registrazione non effettuata!";
+}
+
+$connection->close();
+
 ?>
