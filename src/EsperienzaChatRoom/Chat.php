1310b313c9ee1c923797f439d17ba3590258e56b
st10999 <st10999@iismarconipieralisi.it>
2026-01-26 08:03:30 +0000

    ritrono al pannello

--- src/EsperienzaChatRoom/Chat.php
+++ src/EsperienzaChatRoom/Chat.php
@@ -18,14 +18,15 @@ if((isset($_SESSION['username']) && $_SESSION['username']==true))
 }
 ?>
 
-<h1>Sei Entrato nella ChatRoom!</h1>
-<h3>Qui puoi scrivere un messaggio e visualizzare quelli scritti da altri utenti!</h3>
-<form method="post" action="Chat.php">
-    <label>Scrivi un messaggio...</label><br>
-    <input type="text" name="nome" required>
-    <br><br>
-    <input type="submit" name="crea" value="Invia Messaggio">
-</form>
+    <h1>Sei Entrato nella ChatRoom!</h1>
+    <h3>Qui puoi scrivere un messaggio e visualizzare quelli scritti da altri utenti!</h3>
+    <form method="post" action="Chat.php">
+        <label>Scrivi un messaggio...</label><br>
+        <input type="text" name="nome" required>
+        <br><br>
+        <input type="submit" name="crea" value="Invia Messaggio">
+    </form>
+    <a href="Pannello.php">Torna Al Pannello!</a>
 
 <?php
 
