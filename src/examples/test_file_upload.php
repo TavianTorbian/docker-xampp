f53a4902c6633704e124338c81bd902310dc0691
st10999 <st10999@iismarconipieralisi.it>
2026-02-04 11:32:53 +0000

    dollaro files

--- src/examples/test_file_upload.php
+++ src/examples/test_file_upload.php
@@ -1,9 +1,29 @@
+<?php
+    echo "richiesta POST";
+    var_dump($_POST);
+    if($_POST && isset($_POST['submit'])){
+        var_dump($_FILES);
+        var_dump($_FILES['file1']);
+
+        $contenuto = file_get_contents($_FILES['file1']['tmp_name']);    
+
+        $contenuto2=json_decode($contenuto,true);
+        var_dump($contenuto2);
+
+        foreach($contenuto2 as $key=>$value){
+            var_dump($key);
+            var_dump($value);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <body>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
+        <form action="" method="post" enctype="multipart/form-data">
         Select image to upload:
-            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="file" name="file1" id="file1">
+            <input type="file" name="file2" id="file2">
             <input type="submit" value="Upload Image" name="submit">
         </form>
     </body>
