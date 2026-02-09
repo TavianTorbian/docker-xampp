e6c92acedcd4bf9db029af2d5624317e2a32a29a
st10999 <st10999@iismarconipieralisi.it>
2026-02-09 08:03:25 +0000

    gas

--- src/examples/test_file_upload.php
+++ src/examples/test_file_upload.php
@@ -1,18 +1,40 @@
 <?php
     echo "richiesta POST";
     var_dump($_POST);
-    if($_POST && isset($_POST['submit'])){
+
+    if($_POST && isset($_POST['submit']) && isset($_FILES)){
+
         var_dump($_FILES);
-        var_dump($_FILES['file1']);
 
-        $contenuto = file_get_contents($_FILES['file1']['tmp_name']);    
+        if(isset($_FILES['file1'])){
+            var_dump($_FILES['file1']);
+            $path = $_FILES['file1']['tmp_name'];
 
-        $contenuto2=json_decode($contenuto,true);
-        var_dump($contenuto2);
+            if(file_exists($path)){
+                $contenuto = file_get_contents($_FILES['file1']['tmp_name']);    
 
-        foreach ($contenuto2 as $key => $value){
-            var_dump($key);
-            var_dump($value);
+                $contenuto2=json_decode($contenuto,true);
+                var_dump($contenuto2);
+    
+                foreach ($contenuto2 as $key => $value){
+                    var_dump($key);
+                    var_dump($value);
+                }
+            }else{
+                echo "File 1 non trovato!";
+            }
+        }
+        if(isset($_FILES['file2'])){
+            var_dump($_FILES['file2']);
+            $path = $_FILES['file2']['tmp_name'];
+
+            if(file_exists($path)){
+
+                move_uploaded_file($path,'./mtcars');
+
+            }else{
+                echo "File 2 non trovato!";
+            }
         }
     }
 ?>
