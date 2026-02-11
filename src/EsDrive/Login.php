725d5f1d9c4e6bc38bfc14c3b0c7c4e80a9b9b2e
st10999 <st10999@iismarconipieralisi.it>
2026-02-11 11:33:21 +0000

    login e sing up inpostati

--- src/EsDrive/Login.php
+++ src/EsDrive/Login.php
@@ -1,1 +1,43 @@
 <?php
+session_start(); 
+
+$host = 'db';
+$dbname = 'Drive';
+$user = 'user';
+$dbpassword = 'user';
+$port = 3306;
+
+$connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
+
+if ($connection->connect_error) 
+{
+    die("Errore di connessione: " . $connection->connect_error);
+}
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $connection->prepare("SELECT * FROM utenti WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) 
+    {
+        $row = $result->fetch_assoc();
+        $hashSalvato = $row['password'];
+
+        if (password_verify($password, $hashSalvato)) {
+            session_regenerate_id(true);
+            $_SESSION['email'] = $email;
+            header("Location: Dashboard.php");
+        } else {
+            echo "Password errata!";
+        }
+    } 
+    else 
+    {
+        echo "Utente non trovato!";
+    }
+    $connection->close();
+?>
--- src/EsDrive/SignUp.php
+++ src/EsDrive/SignUp.php
@@ -1,1 +1,52 @@
 <?php
+session_start();
+
+$host = 'db';
+$dbname = 'Drive';
+$user = 'user';
+$dbpassword = 'user';
+$port = 3306;
+
+$connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
+
+if ($connection->connect_error) {
+    die("Errore di connessione: " . $connection->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$conferma_password = $_POST['confermapassword'];
+
+if ($password !== $conferma_password) {
+    echo "Le password non corrispondono!";
+    echo "<br><br>";
+    echo "<a href='SignUp.php'>Torna alla Registrazione!</a>";
+}else{
+
+    $stmt = $connection->prepare("SELECT * FROM utenti WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) 
+    {
+        echo "Utente già presente!";
+        echo "<br><br>";
+        echo "<a href='Login.php'>Torna al Login!</a>";
+    }else{
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $connection->prepare("INSERT INTO utenti (email, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $email, $hash);
+    
+        if ($stmt->execute()) 
+        {
+            header("Location: Dashboard.php");
+        }else{
+            echo "Registrazione non effettuata!";
+        }
+    }
+}
+
+$connection->close();
+
+?>
