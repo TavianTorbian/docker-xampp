9aadfe032ecd4a2e7bb8a640580a51513245694a
st10999 <st10999@iismarconipieralisi.it>
2026-03-04 11:56:02 +0000

    funzia lo stesso senza quella roba li

--- src/EsDrive/Dashboard.php
+++ src/EsDrive/Dashboard.php
@@ -137,15 +137,4 @@ function rinominaFile(form) {
     return true; 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const fileInput = document.querySelector('.file-input');
-    const fileName = document.querySelector('.file-name');
-
-    fileInput.addEventListener('change', () => {
-        if (fileInput.files.length > 0) {
-            fileName.textContent = fileInput.files[0].name;
-        }
-    });
-});
-
 </script>
