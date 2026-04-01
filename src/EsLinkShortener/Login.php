1368614e12144d94437cf13e05dca0de992e291f
st10999 <st10999@iismarconipieralisi.it>
2026-04-01 10:09:42 +0000

    link  login e sign up

---
+++ src/EsLinkShortener/Login.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+
+    $host = 'db';
+    $dbname = 'ChatRoom';
+    $user = 'user';
+    $dbpassword = 'user';
+    $port = 3306;
+
+    $connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
+
+    if ($connection->connect_error) 
+    {
+        die("Errore di connessione: " . $connection->connect_error);
+    }
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $connection->prepare("SELECT * FROM utenti WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) 
+    {
+        $row = $result->fetch_assoc();
+        $hashSalvato = $row['password'];
+
+        if (password_verify($password, $hashSalvato)) {
+            session_regenerate_id(true);
+            $_SESSION['username'] = $username;
+            header("Location: Pannello.php");
+        } else {
+            echo "Password errata!";
+        }
+    } 
+    else 
+    {
+        echo "Utente non trovato!";
+    }
+    $connection->close();
+
+?>
---
+++ src/EsLinkShortener/SignUp.php
@@ -0,0 +1,51 @@
+<?php
+    session_start();
+
+    $host = 'db';
+    $dbname = 'ChatRoom';
+    $user = 'user';
+    $dbpassword = 'user';
+    $port = 3306;
+    
+    $connection = new mysqli($host, $user, $dbpassword, $dbname, $port);
+    
+    if ($connection->connect_error) {
+        die("Errore di connessione: " . $connection->connect_error);
+    }
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $conferma_password = $_POST['confermapassword'];
+
+    if ($password !== $conferma_password) {
+        echo "Le password non corrispondono!";
+        echo "<br><br>";
+        echo "<a href='SignUp.html'>Torna alla Registrazione!</a>";
+    }else{
+
+        $stmt = $connection->prepare("SELECT * FROM utenti WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        if ($result->num_rows > 0) 
+        {
+            echo "Utente già presente!";
+            echo "<br><br>";
+            echo "<a href='Login.html'>Torna al Login!</a>";
+        }else{
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $stmt = $connection->prepare("INSERT INTO utenti (username, password) VALUES (?, ?)");
+            $stmt->bind_param("ss", $username, $hash);
+        
+            if ($stmt->execute()) 
+            {
+                header("Location: Pannello.php");
+            }else{
+                echo "Registrazione non effettuata!";
+            }
+        }
+    }
+
+    $connection->close();
+?>
